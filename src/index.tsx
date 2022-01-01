3340e1c74dd7eabd3b9100cb569ac12bb2141d59
develoQ <develoQ.jp@gmail.com>
2022-01-01 20:30:45 +0900

    add google analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
+import { HtmlHeader } from "./HtmlHeader"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
+    <HtmlHeader />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
